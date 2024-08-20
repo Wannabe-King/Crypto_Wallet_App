d90e85dd439d50cc3782c436fd575b1adc971495
Priyanshu Sidar <sidarsunita2@gmail.com>
2024-08-20 10:09:01 +0530

    deposit page added

---
+++ lib/features/dashboard/bloc/dashboard_bloc.dart
@@ -0,0 +1,150 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:developer';
+import 'package:bloc/bloc.dart';
+import 'package:crypto_eth_wallet/models/transaction_model.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:web3dart/web3dart.dart';
+import 'package:http/http.dart' as http;
+import 'package:web_socket_channel/io.dart';
+
+part 'dashboard_event.dart';
+part 'dashboard_state.dart';
+
+class DashboardBloc extends Bloc<DashboardEvent, DashboardState> {
+  DashboardBloc() : super(DashboardInitial()) {
+    on<DashboardInitialFechEvent>(dashboardInitialFechEvent);
+    on<DashboardDepositEvent>(dashboardDepositEvent);
+    on<DashboardWithdrawEvent>(dashboardWithdrawEvent);
+  }
+
+  List<TransactionModel> transactions = [];
+  Web3Client? _web3Client;
+  late ContractAbi _abiCode;
+  late EthereumAddress _contractAddress;
+  late EthPrivateKey _creds;
+  int balance = 0;
+
+  // Functions
+  late DeployedContract _deployedContract;
+  late ContractFunction _deposit;
+  late ContractFunction _withdraw;
+  late ContractFunction _getBalance;
+  late ContractFunction _getAllTransactions;
+
+  FutureOr<void> dashboardInitialFechEvent(
+      DashboardInitialFechEvent event, Emitter<DashboardState> emit) async {
+    emit(DashboardLoadingState());
+    try {
+      String rpcUrl = "http://127.0.0.1:7545";
+      String socketUrl = "ws://127.0.0.1:7545";
+      String privateKey =
+          "0x5f806fbab7580a21c08303f45ef2327c53d78d7a89a62a850b52f8ba6d9fa454";
+
+      _web3Client = Web3Client(
+        rpcUrl,
+        http.Client(),
+        socketConnector: () {
+          return IOWebSocketChannel.connect(socketUrl).cast<String>();
+        },
+      );
+
+      // getABI
+      String abiFile = await rootBundle
+          .loadString('build/contracts/ExpenseManagerContract.json');
+      var jsonDecoded = jsonDecode(abiFile);
+
+      _abiCode = ContractAbi.fromJson(
+          jsonEncode(jsonDecoded["abi"]), 'ExpenseManagerContract');
+
+      _contractAddress =
+          EthereumAddress.fromHex("0x1C6C5E0Adcd2d0f40E66d6F6Ec4Ff466b7338dA9");
+
+      _creds = EthPrivateKey.fromHex(privateKey);
+
+      // get deployed contract
+      _deployedContract = DeployedContract(_abiCode, _contractAddress);
+      _deposit = _deployedContract.function("deposit");
+      _withdraw = _deployedContract.function("withdraw");
+      _getBalance = _deployedContract.function("getBalance");
+
+      _getAllTransactions = _deployedContract.function("getAllTransactions");
+
+      final transactionsData = await _web3Client!.call(
+          contract: _deployedContract,
+          function: _getAllTransactions,
+          params: []);
+      final balanceData = await _web3Client!
+          .call(contract: _deployedContract, function: _getBalance, params: [
+        EthereumAddress.fromHex("0xec58056550Dc3A60C96EeB220D0862Bb6b2988cb")
+      ]);
+
+      List<TransactionModel> trans = [];
+      for (int i = 0; i < transactionsData[0].length; i++) {
+        TransactionModel transactionModel = TransactionModel(
+            transactionsData[0][i].toString(),
+            transactionsData[1][i].toInt(),
+            transactionsData[2][i],
+            DateTime.fromMicrosecondsSinceEpoch(
+                transactionsData[3][i].toInt()));
+        trans.add(transactionModel);
+      }
+      transactions = trans;
+
+      int bal = balanceData[0].toInt();
+      balance = bal;
+
+      emit(DashboardSuccessState(transactions: transactions, balance: balance));
+    } catch (e) {
+      log(e.toString());
+      emit(DashboardErrorState());
+    }
+  }
+
+  FutureOr<void> dashboardDepositEvent(
+      DashboardDepositEvent event, Emitter<DashboardState> emit) async {
+    try {
+      final transaction = Transaction.callContract(
+          from: EthereumAddress.fromHex(
+              "0xec58056550Dc3A60C96EeB220D0862Bb6b2988cb"),
+          contract: _deployedContract,
+          function: _deposit,
+          parameters: [
+            BigInt.from(event.transactionModel.amount),
+            event.transactionModel.reason
+          ],
+          value: EtherAmount.inWei(BigInt.from(event.transactionModel.amount)));
+
+      final result = await _web3Client!.sendTransaction(_creds, transaction,
+          chainId: 1337, fetchChainIdFromNetworkId: false);
+      log(result.toString());
+      add(DashboardInitialFechEvent());
+    } catch (e) {
+      log(e.toString());
+    }
+  }
+
+  FutureOr<void> dashboardWithdrawEvent(
+      DashboardWithdrawEvent event, Emitter<DashboardState> emit) async {
+    try {
+      final transaction = Transaction.callContract(
+        from: EthereumAddress.fromHex(
+            "0xec58056550Dc3A60C96EeB220D0862Bb6b2988cb"),
+        contract: _deployedContract,
+        function: _withdraw,
+        parameters: [
+          BigInt.from(event.transactionModel.amount),
+          event.transactionModel.reason
+        ],
+      );
+
+      final result = await _web3Client!.sendTransaction(_creds, transaction,
+          chainId: 1337, fetchChainIdFromNetworkId: false);
+      log(result.toString());
+      add(DashboardInitialFechEvent());
+    } catch (e) {
+      log(e.toString());
+    }
+  }
+}
---
+++ lib/features/deposit/deposit.dart
@@ -0,0 +1,77 @@
+import 'package:crypto_eth_wallet/features/dashboard/bloc/dashboard_bloc.dart';
+import 'package:crypto_eth_wallet/models/transaction_model.dart';
+import 'package:crypto_eth_wallet/utils/colors.dart';
+import 'package:flutter/material.dart';
+
+class DepositPage extends StatefulWidget {
+  final DashboardBloc dashboardBloc;
+  const DepositPage({super.key, required this.dashboardBloc});
+
+  @override
+  State<DepositPage> createState() => _DepositPageState();
+}
+
+class _DepositPageState extends State<DepositPage> {
+  final TextEditingController addressController = TextEditingController();
+  final TextEditingController amountController = TextEditingController();
+  final TextEditingController reasonsController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.greenAccent,
+      body: Container(
+        margin: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 80),
+            Text(
+              "Deposit Details",
+              style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 20),
+            // TextField(
+            //   controller: addressController,
+            //   decoration: InputDecoration(hintText: "Enter the Address"),
+            // ),
+            TextField(
+              controller: amountController,
+              decoration: InputDecoration(hintText: "Enter the Amount"),
+            ),
+            TextField(
+              controller: reasonsController,
+              decoration: InputDecoration(hintText: "Enter the Reason"),
+            ),
+            const SizedBox(height: 20),
+            InkWell(
+              onTap: () {
+                widget.dashboardBloc.add(DashboardDepositEvent(
+                    transactionModel: TransactionModel(
+                        addressController.text,
+                        int.parse(amountController.text),
+                        reasonsController.text,
+                        DateTime.now())));
+                Navigator.pop(context);
+              },
+              child: Container(
+                height: 50,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                    color: Colors.green),
+                child: const Center(
+                  child: Text(
+                    "+ DEPOSIT",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
